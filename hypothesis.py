4de031a68692ea5c17f12f215b597108f996e6db
sentdb <systemd.rashadul@hotmail.com>
2024-11-09 04:52:48 -0500

    Update hypothesis.py

--- hypothesis.py
+++ hypothesis.py
@@ -2,24 +2,24 @@ from scipy import stats
 import numpy as np
 
 class trend():
-    def statcalc(df1,df2):
-        stat_hypo, p_value  = stats.ttest_ind(df1["nn_occurences"],df2["vb_occurences"])
+ def statcalc(df1,df2):
+     stat_hypo, p_value  = stats.ttest_ind(df1["nn_occurences"],df2["vb_occurences"])
 
-        # Interpret the results:
-        alpha = 0.05
-        if p_value < alpha:
-            print("Reject the null hypothesis; there is a significant difference between the noun frequency this texts and verbs frequency.")
-        else:
-            print("Fail to reject the null hypothesis; there is no significant difference between the noun frequency this texts and verbs frequency.")
+   # Interpret the results:
+     alpha = 0.05
+     if p_value < alpha:
+        print("Reject the null hypothesis; there is a significant difference between the noun frequency this texts and verbs frequency.")
+     else:
+        print("Fail to reject the null hypothesis; there is no significant difference between the noun frequency this texts and verbs frequency.")
 
-        trends = stats.chi2_contingency([df1["nn_occurences"].head(10),df2["vb_occurences"].head(10)])
+     trends = stats.chi2_contingency([df1["nn_occurences"].head(10),df2["vb_occurences"].head(10)])
 
         #print(f"chi2 statistic:     {chi2:.5g}")
         #print(f"p-value:            {p:.5g}")
         #print(f"degrees of freedom: {dof}")
         #print("expected frequencies:")
         #print(expected)
-        return trends
+     return trends
 
     def shannonDiversity(df):
         print(df[1])
